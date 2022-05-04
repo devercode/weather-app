7e32c7dcf06b3cfab5dc4f70fb52e513f5426a32
Devercode <root@devercode.dev>
2022-05-04 15:47:26 +0700

    feat: complete favorites

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,32 @@
 import axios from "axios";
+import _ from "lodash";
+const keys = [
+  "sqAsoOUJlZgRBCkyfEBrl23jnntaOfuw",
+  "dtsHeugV48INzXPkjfG1ENV74Bb4NGMx",
+  "gvwLIfThiOTtzFNzl2WwPRkKuXA5tTQi",
+  "XVb86zFjDIsFU0yrnZAHepOPM51bs3k9",
+  "cGeBZKrzq1FEJzquy1cyYQfGso5JEDdT",
+  "KgzxgUieAeJSsnuN2GO10mIjsk8vbRho",
+  "Oe3WMvOtZ0laAZg5pSCtQIqxiOOa7Jbe",
+  "pnJvjBIq0gP2NU0le41HfuPZGp3kPfSL",
+  "eaGlYlLqRY0JRcsq0foLZGsu3aVZshxk",
+  "TkEDMtKAGzT7ruveBl6rR2JKGIaTjUqK",
+  "TothQJZ14f0G1sKaf7oIAixGXQnjenaN",
+];
 
 const client = axios.create({
   baseURL: "https://dataservice.accuweather.com",
   params: {
     apikey: "myFAXbBmLQPh5NSD1TD549oTce5Ck2uC",
   },
+});
+
+client.interceptors.request.use((request) => {
+  request.params = {
+    ...request.params,
+    apikey: _.shuffle(keys)[0],
+  };
+  return request;
 });
 
 export const getLocationByGEO = (lat, lon) => {
--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,24 +1,41 @@
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { getForecast1DayByLocation } from "../api";
-
+import { Card, Typography, Stack } from "@mui/material";
 const Location = (props) => {
   const [data, setData] = useState(null);
   console.log(data);
   useEffect(() => {
     getForecast1DayByLocation(props).then((data) => setData(data));
   }, []);
-  return <div>{props.Key}</div>;
+
+  return (
+    <Card>
+      {data && (
+        <Typography>
+          {data.location.EnglishName}, {data.location.Country.EnglishName}
+        </Typography>
+      )}
+      {data && (
+        <Typography>
+          {data && data.DailyForecasts[0].Temperature.Maximum.Value}° C
+        </Typography>
+      )}
+      {data && (
+        <Typography>{data.DailyForecasts[0].Day.ShortPhrase}</Typography>
+      )}
+    </Card>
+  );
 };
 const Favorites = () => {
   const favLocations = useSelector((state) => state.favorites.locations);
   console.log(favLocations);
   return (
-    <div>
+    <Stack spacing={10}>
       {favLocations.map((fav) => (
         <Location {...fav} />
       ))}
-    </div>
+    </Stack>
   );
 };
 
